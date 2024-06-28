b05a03501cc14302cb146d1cf7d1711a012ca84c
Ariciu <vladlaudat@yahoo.com>
2024-06-28 16:52:20 +0300

    88. Merge Sorted Array

---
+++ 88. Merge Sorted Array/Program.cs
@@ -0,0 +1,29 @@
+﻿
+public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int i = 0; int j = 0; int k = 0; ; int[] array = new int[m + n];
+        while (i < m + n)
+        {
+
+            if (m != 0 && n != 0)
+            {
+                if (nums1[j] < nums2[k])
+                {
+                    array[i++] = nums1[j];
+                    if (j < m)
+                        j++;
+                }
+                else
+            if (n != 0)
+                {
+                    array[i++] = nums2[k];
+                    if (k < n)
+                        k++;
+                }
+            }
+        }
+        nums1 = array;
+    }
+}
---
+++ 9. Palindrome Number/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        int mirrored = 0; int x_copy = x;
+        if (x < 0)
+            return false;
+
+        while (x_copy > 0)
+        {
+            mirrored = mirrored * 10 + x_copy % 10;
+            x_copy = x_copy / 10;
+        }
+
+        if (mirrored == x)
+            return true;
+
+        return false;
+    }
+}
