b4fe8e2c3b9c653d84fa0d3963dfcd33fd65b153
Ariciu <vladlaudat@yahoo.com>
2024-06-28 17:06:36 +0300

    27. Remove Element

---
+++ 27. Remove Element/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int k = 0; int[] changedArray = new int[nums.Length];
+
+        foreach (int i in nums)
+        {
+            if (i != val)
+            {
+
+                changedArray[k++] = i;
+            }
+        }
+        nums = changedArray;
+
+        return k;
+    }
+}
