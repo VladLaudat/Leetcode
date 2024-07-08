4ab68a4a56b92345cf97fe062f0d7b35e1855eb7
Ariciu <vladlaudat@yahoo.com>
2024-07-08 14:38:41 +0300

    21. Merge Two Sorted Lists done

--- 21. Merge Two Sorted Lists/Program.cs
+++ 21. Merge Two Sorted Lists/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+  public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int val=0, ListNode next=null) {
+          this.val = val;
+          this.next = next;
+      }
+  }
+
+public class Solution
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode list3 = new ListNode();
+        if (list1 == null)
+            return list2;
+        if (list2 == null)
+            return list1;
+        if (list1.val <= list2.val)
+        {
+            list3.val = list1.val;
+            list1 = list1.next;
+        }
+        else
+        {
+            list3.val = list2.val;
+            list2 = list2.next;
+        }
+        ListNode solution = list3;
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val <= list2.val)
+            {
+                list3.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                list3.next = list2;
+                list2 = list2.next;
+            }
+            list3 = list3.next;
+        }
+
+        while (list1 != null)
+        {
+            list3.next = list1;
+            list1 = list1.next;
+            list3 = list3.next;
+        }
+
+        while (list2 != null)
+        {
+            list3.next = list2;
+            list2 = list2.next;
+            list3 = list3.next;
+        }
+
+        return solution;
+
+    }
+}
--- 21. Merge Two Sorted Lists/obj/Debug/net8.0/21. Merge Two Sorted Lists.AssemblyInfo.cs
+++ 21. Merge Two Sorted Lists/obj/Debug/net8.0/21. Merge Two Sorted Lists.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("21. Merge Two Sorted Lists")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08eb372e2a9a45ac50087c8c2a0b7aeb47dcf628")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84b64d388c284a7cc2027d51c193d551456bd25c")]
 [assembly: System.Reflection.AssemblyProductAttribute("21. Merge Two Sorted Lists")]
 [assembly: System.Reflection.AssemblyTitleAttribute("21. Merge Two Sorted Lists")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
