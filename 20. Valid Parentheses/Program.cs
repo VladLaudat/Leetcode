84b64d388c284a7cc2027d51c193d551456bd25c
Ariciu <vladlaudat@yahoo.com>
2024-07-08 13:49:37 +0300

    20. Valid Parentheses done

---
+++ 20. Valid Parentheses/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+        Dictionary<char,char> opposites = new Dictionary<char,char>();
+        opposites['('] = ')';
+        opposites['['] = ']';
+        opposites['{'] = '}';
+        foreach(char c in s)
+        {
+            if(opposites.Keys.Contains(c))
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                if(stack.Count==0)
+                    return false;
+                if (c != opposites[stack.Peek()])
+                    return false;
+                stack.Pop();
+            }
+        }
+        if (stack.Count == 0)
+            return true;
+        return false;
+    }
+}
