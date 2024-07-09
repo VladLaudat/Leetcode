0c6608d41ff093f33306b53413a9d93a2999bb11
Ariciu <vladlaudat@yahoo.com>
2024-07-09 13:20:33 +0300

    121. Best Time to Buy and Sell done

---
+++ 121. Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int max = 0, valMin = prices[0];
+        if (prices.Length == 1)
+            return 0;
+
+        for(int i=0;i<prices.Length;i++)
+        {
+            if (prices[i]<valMin)
+                valMin = prices[i];
+            else
+            {
+                if (prices[i]-valMin > max)
+                    max = prices[i]-valMin;
+            }
+        }
+        return max;
+    }
+}
