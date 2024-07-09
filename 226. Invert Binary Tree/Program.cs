b9fd1c55115cfb6cf2ff4d9c78dfde95da5cd32e
Ariciu <vladlaudat@yahoo.com>
2024-07-09 14:05:01 +0300

    226. Invert Binary Tree done

---
+++ 226. Invert Binary Tree/Program.cs
@@ -0,0 +1,31 @@
+﻿
+ 
+  public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+      public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root == null)
+            return root;
+        if(!(root.left!=null || root.right!=null))
+        {
+            root.right = InvertTree(root.right);
+            root.left = InvertTree(root.left);
+
+            TreeNode copy = root.left;
+            root.left = root.right;
+            root.right = copy;
+        }
+        return root;
+    }
+}
