55bd653ce8f719701edfcef01ddef7446177904d
Ariciu <vladlaudat@yahoo.com>
2024-07-09 13:49:49 +0300

    125. Valid Palindrome done

---
+++ 125. Valid Palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        string alphaNumericCharacters = "abcdefghijklmnopqrstuvxyz0123456789";
+        s=s.ToLower();
+        int i = 0;
+        while(i<s.Length)
+        {
+            if (!alphaNumericCharacters.Contains(s[i]))
+            {
+                s=s.Remove(i,1);
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        string reversed = "";
+
+        for(i = s.Length-1;i>=0;i--)
+        {
+            reversed+=s[i];
+        }
+
+        if(reversed == s)
+            return true;
+        return false;
+    }
+}
