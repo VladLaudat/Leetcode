dc93293bfebad75301b31bca7c08aa6495140d19
Ariciu <vladlaudat@yahoo.com>
2024-07-11 11:07:35 +0300

    242. Valid Anagram done

---
+++ 242. Valid Anagram/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        Dictionary<char, int> letters= new Dictionary<char, int>();
+
+        foreach(char c in alphabet)
+        {
+            letters.Add(c, 0);
+        }
+
+        foreach(char c in s)
+        {
+            letters[c]++;
+        }
+
+        foreach(char c in t)
+        {
+            letters[c]--;
+        }
+
+        foreach(int value in letters.Values)
+        {
+            if(value!=0)
+                return false;
+        }
+        return true;
+    }
+}
