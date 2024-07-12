946753579a90a7522a14043181e6ef69efcdf0f5
Ariciu <vladlaudat@yahoo.com>
2024-07-12 15:26:08 +0300

    141. Linked List Cycle done

---
+++ 141. Linked List Cycle/Program.cs
@@ -0,0 +1,30 @@
+﻿  public class ListNode {
+     public int val;
+      public ListNode next;
+      public ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+ 
+public class Solution
+{
+    public bool HasCycle(ListNode head)
+    {
+        if(head==null || head.next==null)
+            return false;
+        
+        ListNode fastPointer = head;
+        while(head.next != null && fastPointer!=null && fastPointer.next!=null)
+        {
+            
+            head = head.next;
+            fastPointer = fastPointer.next.next;
+            if (head==fastPointer)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
