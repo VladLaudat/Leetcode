ce68fb70af4f8b68f117509d0790cb3b5a6ed97e
Ariciu <vladlaudat@yahoo.com>
2024-09-09 15:45:28 +0300

    46.Permutations done

--- 46. Permutations/Program.cs
+++ 46. Permutations/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public IList<IList<int>> Permute(int[] nums)
+    {
+        IList<IList<int>> response = new List<IList<int>>();
+        List<int> permutation = new List<int>();
+        bool[] used = new bool[nums.Length];
+        Backtrack(nums, response, 0, permutation, used);
+        return response;
+    }
+    public void Backtrack(int[] nums, IList<IList<int>> response,int level, List<int> permutation, bool[] used)
+    {
+        if (level==nums.Length)
+        {
+            List<int> list = new List<int>(permutation);
+            response.Add(list);
+        }
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!used[i])
+            {
+                permutation.Add(nums[i]); used[i] = true;
+                Backtrack(nums, response, level + 1, permutation, used);
+                permutation.Remove(nums[i]); used[i]= false;
+            }
+        }
+    }
+}
