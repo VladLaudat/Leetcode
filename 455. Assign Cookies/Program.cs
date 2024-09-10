f4b9742b408859ce4914ccc4210418950be968e1
Ariciu <vladlaudat@yahoo.com>
2024-09-10 14:20:41 +0300

    455. Assign Cookies done

---
+++ 455. Assign Cookies/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public int FindContentChildren(int[] g, int[] s)
+    {
+        if (s.Length == 0)
+            return 0;
+        int result = 0;
+        for (int i=0; i<g.Length; i++)
+        {
+            int smallestCookie = int.MaxValue;
+            int smallestCookieIndex = -1;
+            for(int j=0;j<s.Length;j++)
+            {
+                if (s[j] < smallestCookie)
+                {
+                    if (g[i] <= s[j])
+                    { 
+                        smallestCookie = s[j];
+                        smallestCookieIndex = j;
+                    }
+                }
+            }
+            if (smallestCookieIndex != -1)
+            { 
+                s[smallestCookieIndex] = 0;
+                result++;
+            }
+        }
+        return result;
+    }
+}
