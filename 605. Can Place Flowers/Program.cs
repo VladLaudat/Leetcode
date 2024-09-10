97e6edb901be28f05c1a57ffa58bd8655ab4102f
Ariciu <vladlaudat@yahoo.com>
2024-09-10 15:04:19 +0300

    605. Can Place Flowers done

---
+++ 605. Can Place Flowers/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Solution
+{
+    public bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        if(n>flowerbed.Length) return false;
+
+        if(flowerbed.Length==1)
+            if (flowerbed[0]==0)
+                return true;
+
+        int i = 0;
+        while(i<flowerbed.Length)
+        {
+            if (flowerbed[i] == 0)
+            {
+                if (i == 0)
+                {
+                    if (flowerbed[i + 1] == 0)
+                       { n--; flowerbed[i] = 1; }
+                }
+                else
+            if (i == flowerbed.Length - 1)
+                {
+                    if (flowerbed[i - 1] == 0)
+                    { n--; flowerbed[i] = 1; }
+                }
+                else
+            if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0)
+                { n--; flowerbed[i] = 1; }
+            }
+            i++;
+        }
+
+        return n > 0 ? false : true;
+    }
+}
