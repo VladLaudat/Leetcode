98d9b4981ce399c0dbc5b38fd01a649aa21c4fb1
Ariciu <vladlaudat@yahoo.com>
2024-09-11 14:30:14 +0300

    53. Maximum Subarray done

---
+++ 53. Maximum Subarray/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int MaxSubArray(int[] nums)
+    {
+        int i = 0, j = 0;
+        int sum = 0, maxSum = int.MinValue;
+        while(j<nums.Length)
+        {
+            sum += nums[j];
+            if (nums[j]>sum)
+            {
+                i = j;
+                sum = nums[j];
+            }
+            maxSum = Math.Max(maxSum, sum);
+            j++;
+        }
+        return maxSum;
+    }
+}
