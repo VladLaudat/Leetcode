f7237b043160757aed112f42e65ffdc0460c776b
Ariciu <vladlaudat@yahoo.com>
2024-09-11 18:04:41 +0300

    55. Jump Game done

---
+++ 55. Jump Game/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+public class Solution
+{
+    public bool CanJump(int[] nums)
+    {
+        int i = 0;
+        foreach(int n in nums)
+        {
+            if (i < 0) return false;
+            else
+                if (n > i)
+                i = n;
+            i--;
+        }
+        return true;
+    }
+}
