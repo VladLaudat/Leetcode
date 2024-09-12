51a09c12aedb133ff16f8c5bb64e2e8969dd1a99
Ariciu <vladlaudat@yahoo.com>
2024-09-12 16:15:20 +0300

    746. Min Cost Climbing Stairs done

---
+++ 746. Min Cost Climbing Stairs/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int MinCostClimbingStairs(int[] cost)
+    {
+        int i = 0;
+        int minCost = 0;
+        if (cost[0] > cost[1])
+            i = 1;
+        minCost = Math.Min(cost[0], cost[1]);
+
+        while (i<cost.Length-2)
+        {
+            minCost = minCost + Math.Min(cost[i + 1], cost[i + 2]);
+            if (cost[i+1] < cost[i+2])
+                i=i+1;
+            else
+                i = i+2;
+        }
+    }
+}
