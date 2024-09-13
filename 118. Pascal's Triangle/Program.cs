68826bca75c7a6e97dd2ff20bf0b2d50031eb8c5
Ariciu <vladlaudat@yahoo.com>
2024-09-13 18:51:37 +0300

    198. House Robber done

--- 118. Pascal's Triangle/Program.cs
+++ 118. Pascal's Triangle/Program.cs
@@ -1,14 +1,5 @@
 ﻿public class Solution
 {
-    public static void Main(string[] args)
-    {
-        Solution s = new Solution();
-        IList<IList<int>> ints = s.Generate(3);
-        foreach (List<int> ints1 in ints)
-        {
-            Console.WriteLine(string.Join(" ", ints1));
-        }
-    }
     public IList<IList<int>> Generate(int numRows)
     {
         IList<IList<int>> list = new List<IList<int>>();
@@ -22,11 +13,11 @@
         if (numRows == 2)
             return list;
 
-        for(int i=2;i<numRows;i++)
+        for (int i = 2; i < numRows; i++)
         {
             List<int> row = new List<int>() { 1 };
             for (int j = 1; j < i; j++)
-                row.Add(list[i - 1][j-1] + list[i - 1][j]);
+                row.Add(list[i - 1][j - 1] + list[i - 1][j]);
             row.Add(1);
             list.Add(row);
         }
---
+++ 198. House Robber/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public int Rob(int[] nums)
+    {
+        if(nums.Length==1) return nums[0];
+
+        int max = nums[nums[0]];
+
+        for (int i=2;i<nums.Length;i++)
+        {
+            nums[i] += max;
+            max = Math.Max(max, nums[i-1]);
+        }
+        Console.WriteLine(string.Join(",", nums));
+        return Math.Max(nums[nums.Length - 1], nums[nums.Length-2]);
+    }
+}
